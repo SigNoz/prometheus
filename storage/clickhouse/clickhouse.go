6929fa4481f272d90afb7c8ce0430134831547d6
Aniket <aniket@signoz.io>
2025-01-12 15:38:32 +0530

    feat: added max_rows_to_group_by and max_result_rows| 1196

--- storage/clickhouse/clickhouse.go
+++ storage/clickhouse/clickhouse.go
@@ -243,6 +243,16 @@ func (ch *clickHouse) addClickHouseSettings(ctx context.Context) context.Context
 		settings["max_bytes_to_read"] = os.Getenv("ClickHouseMaxBytesToRead")
 	}
 
+	if ctx.Value("max_result_rows") != nil && ctx.Value("result_overflow_mode") != nil {
+		settings["max_result_rows"] = ctx.Value("max_result_rows").(int)
+		settings["result_overflow_mode"] = ctx.Value("result_overflow_mode")
+	}
+
+	if ctx.Value("max_rows_to_group_by") != nil && ctx.Value("result_overflow_mode") != nil {
+		settings["max_rows_to_group_by"] = ctx.Value("max_rows_to_group_by").(int)
+		settings["result_overflow_mode"] = ctx.Value("result_overflow_mode")
+	}
+
 	ctx = clickhouse.Context(ctx, clickhouse.WithSettings(settings))
 	return ctx
 }
